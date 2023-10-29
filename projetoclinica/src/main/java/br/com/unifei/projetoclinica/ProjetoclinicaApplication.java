28a9c4ea6c6d452aa1d68999ed4a686410e59999
Bruna Delmouro <brunadelmouro@gmail.com>
2023-10-28 22:23:10 -0300

    Starting project structure

---
+++ projetoclinica/src/main/java/br/com/unifei/projetoclinica/ProjetoclinicaApplication.java
@@ -0,0 +1,13 @@
+package br.com.unifei.projetoclinica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoclinicaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoclinicaApplication.class, args);
+    }
+
+}
---
+++ projetoclinica/src/test/java/br/com/unifei/projetoclinica/ProjetoclinicaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unifei.projetoclinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoclinicaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
