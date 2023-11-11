b16cf5e38ff02d463bfbb08cbf6d61413788c34b
Bruna Delmouro <brunadelmouro@gmail.com>
2023-11-11 13:44:26 -0300

    Formatting file with google formatter

--- projetoclinica/src/main/java/br/com/unifei/projetoclinica/ProjetoclinicaApplication.java
+++ projetoclinica/src/main/java/br/com/unifei/projetoclinica/ProjetoclinicaApplication.java
@@ -12,57 +12,60 @@ import java.util.List;
 @SpringBootApplication
 public class ProjetoclinicaApplication implements CommandLineRunner {
 
-    @Autowired
-    ClinicRepository clinicRepository;
+  @Autowired ClinicRepository clinicRepository;
 
-    @Autowired
-    UserRepository userRepository;
+  @Autowired UserRepository userRepository;
 
-    @Autowired
-    VeterinarianRepository veterinarianRepository;
+  @Autowired VeterinarianRepository veterinarianRepository;
 
-    @Autowired
-    PatientRepository patientRepository;
+  @Autowired PatientRepository patientRepository;
 
-    @Autowired
-    GuardianRepository guardianRepository;
+  @Autowired GuardianRepository guardianRepository;
 
-    public static void main(String[] args) {
-        SpringApplication.run(ProjetoclinicaApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(ProjetoclinicaApplication.class, args);
+  }
 
-    @Override
-    public void run(String... args) throws Exception {
-        //// TODO: 11/11/2023 password
-        Clinic clinic = Clinic.builder().name("UniVet").phone("359998751").address("Rua D. A. Chaves, 315").build();
+  @Override
+  public void run(String... args) throws Exception {
+    //// TODO: 11/11/2023 password
+    Clinic clinic =
+        Clinic.builder().name("UniVet").phone("359998751").address("Rua D. A. Chaves, 315").build();
 
-        clinicRepository.save(clinic);
+    clinicRepository.save(clinic);
 
-        User user = User.builder().name("Márcia").email("marcia@gmail.com").clinic(clinic).build();
+    User user = User.builder().name("Márcia").email("marcia@gmail.com").clinic(clinic).build();
 
-        // relationship is bidirectional and JPA/Hibernate can manage the relationship based on the annotation
-        // is not required, but it can be helpful for comprehension
-        //clinic.getUsers().add(user);
+    // relationship is bidirectional and JPA/Hibernate can manage the relationship based on the
+    // annotation
+    // is not required, but it can be helpful for comprehension
+    // clinic.getUsers().add(user);
 
-        userRepository.save(user);
+    userRepository.save(user);
 
-        // ----------------------
+    // ----------------------
 
-        Veterinarian veterinarian = Veterinarian.builder().name("Júlia").specialty("Dogs").clinic(clinic).build();
+    Veterinarian veterinarian =
+        Veterinarian.builder().name("Júlia").specialty("Dogs").clinic(clinic).build();
 
-        veterinarianRepository.save(veterinarian);
+    veterinarianRepository.save(veterinarian);
 
-        // ----------------------
+    // ----------------------
 
-        Guardian guardian = Guardian.builder().name("Bruna").email("bruna@gmail.com").build();
+    Guardian guardian = Guardian.builder().name("Bruna").email("bruna@gmail.com").build();
 
-        guardianRepository.save(guardian);
+    guardianRepository.save(guardian);
 
-        // ----------------------
-        Patient patient = Patient.builder().name("Spike").breed("Mixed").imageUrl("S3_URL").clinic(clinic).guardian(guardian).build();
+    // ----------------------
+    Patient patient =
+        Patient.builder()
+            .name("Spike")
+            .breed("Mixed")
+            .imageUrl("S3_URL")
+            .clinic(clinic)
+            .guardian(guardian)
+            .build();
 
-        patientRepository.save(patient);
-
-    }
-
+    patientRepository.save(patient);
+  }
 }
